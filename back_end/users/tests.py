364d1c6bd4eed7c0bf3f12f8dfdd3aeac49f046b
domenico3622 <128099390+domenico3622@users.noreply.github.com>
2024-10-27 11:23:20 +0100

    setup UserTestCase and test_create_user_success
    create test to verify the correct creation of users with the function "create_user"

--- back_end/users/tests.py
+++ back_end/users/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import User
+from django.urls import reverse
+import json
 
-# Create your tests here.
+class CreateUserTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('create_user')  # Assicurati che questo corrisponda al tuo URL
+
+    def test_create_user_success(self):
+        data = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "john.doe@example.com",
+            "password": "securepassword123"
+        }
+        response = self.client.post(reverse('create_user'), json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(User.objects.count(), 1)
+
+    
