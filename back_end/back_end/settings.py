1b14b8fc209f1adebfc797ad2e5d0f485f4f4865
FadeFox42 <fede.fil03@gmail.com>
2024-11-01 17:40:53 +0100

    SIP-75 Added CORS

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',
     'users',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # URL del frontend
+    "http://127.0.0.1:5173",  # Alternativa per localhost
 ]
 
 ROOT_URLCONF = 'back_end.urls'
--- back_end/back_end/urls.py
+++ back_end/back_end/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#from users import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    #path('create_user', views.create_user, name='create_user'),
 ]
