6196aa9658b0af48b23b5e12298efe4b7eb14026
Pasquale Tudda <84407165+ryuk4real@users.noreply.github.com>
2024-11-09 23:52:52 +0100

    SIP-84 Changed saved papers path

--- back_end/papers/models.py
+++ back_end/papers/models.py
@@ -10,7 +10,7 @@ class Paper(models.Model):
     ]
 
     title = models.CharField(max_length=200)
-    paper_file = models.FileField(upload_to='books/', null=True, blank=True)
+    paper_file = models.FileField(upload_to='papers_pdf/', null=True, blank=True)
     conference = models.ForeignKey(Conference, on_delete=models.CASCADE, related_name="papers")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="papers")
     status = models.CharField(max_length=20, choices=STATUS)
