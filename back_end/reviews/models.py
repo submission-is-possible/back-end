0298b2cbca705be0cf463dd5cd8cc795becb37bd
domenico3622 <128099390+domenico3622@users.noreply.github.com>
2024-11-11 19:26:35 +0100

    creation reviews model

---
+++ back_end/reviews/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from papers.models import Paper  # Assicurati che l'import sia corretto
+from users.models import User
+
+class Review(models.Model):
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    comment_text = models.TextField()
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"Review for {self.paper.title} by {self.user.first_name} {self.user.last_name} - Score: {self.score}"
