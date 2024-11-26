739217e854af20830de3f2e5763d4f408293c2f5
domenico3622 <128099390+domenico3622@users.noreply.github.com>
2024-11-26 19:49:43 +0100

    add conference_field to new models

--- back_end/assign_paper_reviewers/models.py
+++ back_end/assign_paper_reviewers/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from papers.models import Paper
 from users.models import User
+from conference.models import Conference
 
 class PaperReviewAssignment(models.Model):
     STATUS_CHOICES = [
@@ -11,6 +12,7 @@ class PaperReviewAssignment(models.Model):
 
     paper = models.ForeignKey(Paper, on_delete=models.CASCADE, related_name="review_assignments")
     reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="assigned_papers")
+    conference = models.ForeignKey(Conference, on_delete=models.CASCADE, related_name="review_assignments")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='assigned')
     #assigned_at = models.DateTimeField(auto_now_add=True)
     #updated_at = models.DateTimeField(auto_now=True)
