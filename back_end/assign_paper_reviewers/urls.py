c97f23dfa7d4f357c41fa61e25c665fd38d0c125
domenico3622 <128099390+domenico3622@users.noreply.github.com>
2024-11-30 16:44:06 +0100

    add url remove

--- back_end/assign_paper_reviewers/urls.py
+++ back_end/assign_paper_reviewers/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('assign_reviewer_to_paper/', views.assign_reviewer_to_paper, name='assign_reviewer_to_paper'),
+    path('remove_reviewer_from_paper/', views.remove_reviewer_from_paper, name='remove_reviewer_from_paper'),
 ]
--- back_end/back_end/urls.py
+++ back_end/back_end/urls.py
@@ -48,6 +48,4 @@ urlpatterns = [
     #Swagger and Redoc paths for accessing documentation
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-
 ]
