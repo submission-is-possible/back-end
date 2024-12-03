adc64446cc6f11c8321dce78fe0bddd138e8d4d4
domenico3622 <128099390+domenico3622@users.noreply.github.com>
2024-12-03 19:11:49 +0100

    modify preference scheme

--- back_end/preferences/models.py
+++ back_end/preferences/models.py
@@ -3,9 +3,14 @@ from users.models import User
 from papers.models import Paper
 
 class Preference(models.Model):
+    PREFERENCE_CHOICES = [
+        ('not_interested', 'Not Interested'),
+        ('interested', 'Interested')
+    ]
     paper = models.ForeignKey(Paper, on_delete=models.CASCADE, related_name="preferences")
     reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="preferences")
-    preference = models.IntegerField()
+    # not interested, interested
+    preference = models.CharField(max_length=20, choices=PREFERENCE_CHOICES)
 
     def __str__(self):
         return f"preference for {self.paper} of {self.reviewer} score is: {self.preference})"
