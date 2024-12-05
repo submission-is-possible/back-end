98599ab4e8b18f2835b5c46425695110aaf5a27e
nicolearnieri <nicolearnieri@gmail.com>
2024-12-05 09:31:48 +0100

    rifatto

--- back_end/reviews/urls.py
+++ back_end/reviews/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create_review/', views.create_review, name='create_review'),
     path('update_review/<int:review_id>/', views.update_review, name='update_review'),
     path('delete_review/<int:review_id>/', views.delete_review, name='delete_review'),
+    path('hasbeenreviewed/', views.has_been_reviewed, name='has_been_reviewed'),
 ]
